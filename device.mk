e1dd92b31c063ba0258073d2ec37759f8caff608
Zips <hcolmenares@gmail.com>
2013-11-20 16:04:12 -0500

    Bring m7tmo into Omni
    Change-Id: Icbc696f18021619f85bbb1e00b859d1f4cedc4ee

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 # call the proprietary setup
-$(call inherit-product-if-exists, vendor/htc/m7-gsm/m7-gsm-vendor.mk)
+$(call inherit-product-if-exists, vendor/htc/m7tmo/m7tmo-vendor.mk)
 
 # overlays
 DEVICE_PACKAGE_OVERLAYS += device/htc/m7tmo/overlay
@@ -25,3 +25,7 @@ DEVICE_PACKAGE_OVERLAYS += device/htc/m7-common/overlay-gsm
 
 # Inherit from m7-common
 $(call inherit-product, device/htc/m7-common/m7-common.mk)
+
+#OmniTorch
+ PRODUCT_PACKAGES += \
+        OmniTorch
